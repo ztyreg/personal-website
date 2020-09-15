e3141010f970841081389a31ddbf9e67af7fa008
Ethan <ethzheng[at]gmail[dot]com>
2020-09-15 12:07:27 -0500

    update images

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -118,3 +118,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Look like no local file.")
