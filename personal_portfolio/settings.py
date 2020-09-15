13e36f316e79ce17df0f1bf55fc1c633c3ab41c2
Ethan <ztyreg@gmail.com>
2020-09-15 02:25:07 +0000

    remove cache

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*^i6%fojh7qgm413b_4zi_&+y+e^o%j=04v^w&a&-opagyf+me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ztyreg.pythonanywhere.com']
 
 # Application definition
 
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
